175be091d51f659f5b4e8a196598461d782903a1
Derek Baker <bakerdp@sunyit.edu>
2018-11-04 08:50:16 -0500

    Removed rounded edges from nav buttons

--- javascripts/index.vue.navbar.js
+++ javascripts/index.vue.navbar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
     Vue.component('navbar-button', { 
         props: ['text', 'identifier', 'target', 'buttonclasses', 'glyphclasses'],
         template: `<div class="btn-group" role="group">
-                        <button v-bind:id='identifier' class="btn btn-md" 
+                        <button style="border-radius:0;" v-bind:id='identifier' class="btn btn-md" 
                             v-bind:class='buttonclasses' data-toggle="modal" data-target="" 
                             v-bind:data-target='target' type="button">
                             {{ text }} 
