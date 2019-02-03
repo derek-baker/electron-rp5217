c82873127fbbd5c2a5eba380d3b6b99421a407d1
Derek Baker <bakerdp@sunyit.edu>
2019-02-03 10:53:05 -0500

    Added code for auto-updates

--- main.js
+++ main.js
@@ -105,6 +105,12 @@ ipcMain.on('stateResponse', (event, data) => {
 // This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 // Quit when all windows are closed.
+
+app.on('ready', function()  {
+	autoUpdater.checkForUpdatesAndNotify();
+});
+  
+
 app.on('window-all-closed', function () {
 	// On OS X it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
