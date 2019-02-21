f7b34e27eab6115432eaf2f08cc23580ceed34d9
dbaker@sdgnys.com <dbaker@sdgnys.com>
2019-02-21 09:25:56 -0500

    Refactored and updated contact enpoint

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 "use strict";
 
+const pdfCreationEndpoint = 'createPdf';
+const contactEndpoint = 'contactSdg';
+
 const envConfigs = {
     dev : {
-        pdfCreationEndpoint: 'http://localhost:8080/createPdf',
-        contactEndpoint: 'http://localhost:8080/contact'
+        pdfCreationUrl: `http://localhost:8080/${pdfCreationEndpoint}`,
+        contactUrl: `http://localhost:8080/${contactEndpoint}`
     },
     // test : {
-    //     pdfCreationEndpoint: 'http://localhost:8080/createPdf',
-    //     contactEndpoint: 'http://localhost:8080/contact'
+    //     pdfCreationUrl: `http://localhost:8080/createPdf`,
+    //     contactUrl: `http://localhost:8080/contact`
     // },
     prod : {
-        pdfCreationEndpoint: 'https://systemsdevelopmentgroup.com/createPdf',
-        contactEndpoint: 'https://systemsdevelopmentgroup.com/contact'
+        pdfCreationUrl: `https://systemsdevelopmentgroup.com/${pdfCreationEndpoint}`,
+        contactUrl: `https://systemsdevelopmentgroup.com/${contactEndpoint}`
     }
 };
 
