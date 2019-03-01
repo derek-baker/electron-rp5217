81a8dbe50daa54db92a44b2312e6532f43c9a938
dbaker@sdgnys.com <dbaker@sdgnys.com>
2019-03-01 13:26:18 -0500

    Finalized tests

--- test/spec.IPC.js
+++ test/spec.IPC.js
@@ -1,121 +1,121 @@
-// "use strict";
+"use strict";
 
-// const Application = require('spectron').Application;
-// const assert = require('assert');
-// const electronPath = require('electron'); 
-// const path = require('path');
-
-
-// describe('Spectron Intra-App IPC Integration Tests', function () {
-//     this.timeout(100000)
-
-//     beforeEach(function () {
-//         this.app = new Application({            
-//             path: electronPath,
-//             // args tells spectron to look and use the main.js file and the package.json located 1 level above.
-//             args: [path.join(__dirname, '..')],
-//             env: {
-//                 NODE_ENV: "test"
-//             },
-//             requireName: "nodeRequire"
-//         });
-//         return this.app.start()
-//     });
-
-//     afterEach(function () {
-//         if (this.app && this.app.isRunning()) {
-//             return this.app.stop()
-//         }
-//     });
-
-//     it('shows an initial window', function () {
-//         return this.app.client.getWindowCount()
-//             .then( function(count) { assert.strictEqual(count, 1); } 
-//         );        
-//     });
-
-//     // NOTE: For the tests below, the intention was to test that clicking the buttons (at minimum)
-//     //       results in IPC communication that triggers native file dialogs, and the sending of data.
-//     //       I was unable to test the sending of data, and we can't drive native file dialogs with Spectron. 
-
-//     it('Top open button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         const eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('openFile', eventCallback(result));
-
-//         return this.app.client.waitUntilWindowLoaded().$('#importDataButton').click()
-//                    .then( (response) => { assert.strictEqual(result.actual, expected); });        
-//     });
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); 
+const path = require('path');
 
 
-//     it('Bottom open button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         const eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('openFile', eventCallback(result));
+describe('Spectron Intra-App IPC Integration Tests', function () {
+    this.timeout(100000)
 
-//         return this.app.client.waitUntilWindowLoaded().$('#importDataButtonBottom').click()
-//                    .then( (response) => { assert.strictEqual(result.actual, expected); });        
-//     });
+    beforeEach(function () {
+        this.app = new Application({            
+            path: electronPath,
+            // args tells spectron to look and use the main.js file and the package.json located 1 level above.
+            args: [path.join(__dirname, '..')],
+            env: {
+                NODE_ENV: "test"
+            },
+            requireName: "nodeRequire"
+        });
+        return this.app.start()
+    });
+
+    afterEach(function () {
+        if (this.app && this.app.isRunning()) {
+            return this.app.stop()
+        }
+    });
+
+    it('shows an initial window', function () {
+        return this.app.client.getWindowCount()
+            .then( function(count) { assert.strictEqual(count, 1); } 
+        );        
+    });
+
+    // NOTE: For the tests below, the intention was to test that clicking the buttons (at minimum)
+    //       results in IPC communication that triggers native file dialogs, and the sending of data.
+    //       I was unable to test the sending of data, and we can't drive native file dialogs with Spectron. 
+
+    it('Top open button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        const eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('openFile', eventCallback(result));
+
+        return this.app.client.waitUntilWindowLoaded().$('#importDataButton').click()
+                   .then( (response) => { assert.strictEqual(result.actual, expected); });        
+    });
 
 
-//     it('Top save button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         const eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('save', eventCallback(result));
+    it('Bottom open button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        const eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('openFile', eventCallback(result));
 
-//         return this.app.client.waitUntilWindowLoaded().$('#saveFileButton').click()
-//                    .then( (response) => { assert.strictEqual(result.actual, expected); });        
-//     });
+        return this.app.client.waitUntilWindowLoaded().$('#importDataButtonBottom').click()
+                   .then( (response) => { assert.strictEqual(result.actual, expected); });        
+    });
 
 
-//     it('Bottom save button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         const eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('save', eventCallback(result));
+    it('Top save button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        const eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('save', eventCallback(result));
 
-//         return this.app.client.waitUntilWindowLoaded().$('#saveFileButtonBottom').click()
-//                    .then( (response) => { assert.strictEqual(result.actual, expected); });        
-//     });
+        return this.app.client.waitUntilWindowLoaded().$('#saveFileButton').click()
+                   .then( (response) => { assert.strictEqual(result.actual, expected); });        
+    });
+
+
+    it('Bottom save button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        const eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('save', eventCallback(result));
+
+        return this.app.client.waitUntilWindowLoaded().$('#saveFileButtonBottom').click()
+                   .then( (response) => { assert.strictEqual(result.actual, expected); });        
+    });
 
     
-//     it('Top save-as button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         let eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('save-dialog', eventCallback(result));
+    it('Top save-as button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        let eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('save-dialog', eventCallback(result));
         
-//         return this.app.client.waitUntilWindowLoaded().$('#saveAsFileButton').click()
-//                 .then( (response) => {
-//                     assert.strictEqual(result.actual, expected);
-//                 });        
-//     });
+        return this.app.client.waitUntilWindowLoaded().$('#saveAsFileButton').click()
+                .then( (response) => {
+                    assert.strictEqual(result.actual, expected);
+                });        
+    });
 
 
-//     it('Bottom save-as button triggers valid inter-process communication', function() {
-//         let result = {actual: ''};
-//         const expected = 'recieved'
-//         const eventCallback = function(val) {
-//             val.actual = expected;
-//         } 
-//         this.app.electron.remote.ipcMain.on('save-dialog', eventCallback(result));
+    it('Bottom save-as button triggers valid inter-process communication', function() {
+        let result = {actual: ''};
+        const expected = 'recieved'
+        const eventCallback = function(val) {
+            val.actual = expected;
+        } 
+        this.app.electron.remote.ipcMain.on('save-dialog', eventCallback(result));
 
-//         return this.app.client.waitUntilWindowLoaded().$('#saveAsFileButtonBottom').click()
-//                    .then( (response) => { assert.strictEqual(result.actual, expected); });        
-//     });
+        return this.app.client.waitUntilWindowLoaded().$('#saveAsFileButtonBottom').click()
+                   .then( (response) => { assert.strictEqual(result.actual, expected); });        
+    });
 
-//     return;
-// });
+    return;
+});
--- test/spec.main.filesystem.js
+++ test/spec.main.filesystem.js
@@ -1,62 +1,62 @@
-// "use strict";
+"use strict";
 
-// const assert = require('assert');
-// const fs = require('fs');
-// const { readFile, saveFile } = require('../mainProcess.modules/main.filesystem');
-// const path = require('path');
+const assert = require('assert');
+const fs = require('fs');
+const { readFile, saveFile } = require('../mainProcess.modules/main.filesystem');
+const path = require('path');
 
 
-// describe('Intra-App Filesystem Integration Tests', function () {
-//     this.timeout(10000)
+describe('Intra-App Filesystem Integration Tests', function () {
+    this.timeout(10000)
 
-//     beforeEach(function () {        
-//     });
+    beforeEach(function () {        
+    });
 
-//     afterEach(function () {        
-//     });    
+    afterEach(function () {        
+    });    
 
-//     it('Can WRITE a file', async function() {
-//         // Arrange
-//         const mockData = {
-//             swisCode:"123456",propertyLocationStreetNumber:"1773",propertyLocationStreetName:"1222",propertyLocationCityTown:"122ddd",propertyLocationVillage:"1133ss",propertyLocationZipcode:"1",buyerLastNameCompanyOne:"145",buyerFirstNameOne:"1",buyerLastNameCompanyTwo:"1sssss",buyerFirstNameTwo:"1",taxAddressBuyerLastNameCompany:"1",taxAddressBuyerFirstName:"11",taxAddressStreetNumberAndName:"122",taxAddressCityTown:"",taxAddressState:"11",taxAddressZipCode:"",fourNumberOfParcels:"4",fourPartOfParcelCheckbox:false,fourFrontFeet:"1",fourDepth:"1",fourAcres:"1",fourSubDivAuthExists:false,fourSubDivApprovalRequired:false,fourParcelApprovedWithMap:false,sellerNameLastNameCompany:"1",sellerNameFirstName:"1",sellerNameLastNameCompanyTwo:"1",sellerNameFirstNameTwo:"1",propertyUseSelect:"3",ownershipIsCondo:null,constructionOnVacant:null,locatedWithinAg:null,buyerReceivedDisclosureNotice:null,saleContractDate:"0001-01-01",saleTransferDate:"0001-01-01",salePrice:"12,222,222",salePersonalPropertyVal:"222,222",saleConditionComments:"1",saleInfoCheckA:false,saleInfoCheckB:false,saleInfoCheckC:false,saleInfoCheckD:false,saleInfoCheckE:false,saleInfoCheckF:false,saleInfoCheckG:false,saleInfoCheckH:false,saleInfoCheckI:false,saleInfoCheckJ:true,assessmentRollYear:"12",assessmentPropClassFirstInput:"100",assessmentTotalValue:"33,333,333",assessmentSchoolDistrict:"1",taxMapIdOne:"1",taxMapIdTwo:"1",taxMapIdThree:"1",taxMapIdFour:"1",contactInfoLastName:"1",contactInfoFirstName:"1",contactInfoAreaCode:"133",contactInfoPhoneNumber:"133-3333",contactInfoStreetNumber:"1",contactInfoStreetName:"1",contactInfoCityTown:"1",contactInfoState:"1",contactInfoZipCode:"1",contactAttorneyInfoLastName:"1",contactInfoAttorneyFirstName:"1",contactInfoAttorneyAreaCode:"133",contactInfoAttorneyPhoneNum:"133-3333",validationCounterForNumberOfParcels:false
-//         };
-//         const filePath = path.resolve(__dirname, 'fileForWriteTest.sdg');
-//         let actual;
-//         const expected = JSON.stringify(mockData);
+    it('Can WRITE a file', async function() {
+        // Arrange
+        const mockData = {
+            swisCode:"123456",propertyLocationStreetNumber:"1773",propertyLocationStreetName:"1222",propertyLocationCityTown:"122ddd",propertyLocationVillage:"1133ss",propertyLocationZipcode:"1",buyerLastNameCompanyOne:"145",buyerFirstNameOne:"1",buyerLastNameCompanyTwo:"1sssss",buyerFirstNameTwo:"1",taxAddressBuyerLastNameCompany:"1",taxAddressBuyerFirstName:"11",taxAddressStreetNumberAndName:"122",taxAddressCityTown:"",taxAddressState:"11",taxAddressZipCode:"",fourNumberOfParcels:"4",fourPartOfParcelCheckbox:false,fourFrontFeet:"1",fourDepth:"1",fourAcres:"1",fourSubDivAuthExists:false,fourSubDivApprovalRequired:false,fourParcelApprovedWithMap:false,sellerNameLastNameCompany:"1",sellerNameFirstName:"1",sellerNameLastNameCompanyTwo:"1",sellerNameFirstNameTwo:"1",propertyUseSelect:"3",ownershipIsCondo:null,constructionOnVacant:null,locatedWithinAg:null,buyerReceivedDisclosureNotice:null,saleContractDate:"0001-01-01",saleTransferDate:"0001-01-01",salePrice:"12,222,222",salePersonalPropertyVal:"222,222",saleConditionComments:"1",saleInfoCheckA:false,saleInfoCheckB:false,saleInfoCheckC:false,saleInfoCheckD:false,saleInfoCheckE:false,saleInfoCheckF:false,saleInfoCheckG:false,saleInfoCheckH:false,saleInfoCheckI:false,saleInfoCheckJ:true,assessmentRollYear:"12",assessmentPropClassFirstInput:"100",assessmentTotalValue:"33,333,333",assessmentSchoolDistrict:"1",taxMapIdOne:"1",taxMapIdTwo:"1",taxMapIdThree:"1",taxMapIdFour:"1",contactInfoLastName:"1",contactInfoFirstName:"1",contactInfoAreaCode:"133",contactInfoPhoneNumber:"133-3333",contactInfoStreetNumber:"1",contactInfoStreetName:"1",contactInfoCityTown:"1",contactInfoState:"1",contactInfoZipCode:"1",contactAttorneyInfoLastName:"1",contactInfoAttorneyFirstName:"1",contactInfoAttorneyAreaCode:"133",contactInfoAttorneyPhoneNum:"133-3333",validationCounterForNumberOfParcels:false
+        };
+        const filePath = path.resolve(__dirname, 'fileForWriteTest.sdg');
+        let actual;
+        const expected = JSON.stringify(mockData);
         
-//         // Act
-//         await saveFile(filePath, JSON.stringify(mockData), undefined);
-//         actual = fs.readFileSync(filePath, {encoding: "utf-8"}); 
+        // Act
+        await saveFile(filePath, JSON.stringify(mockData), undefined);
+        actual = fs.readFileSync(filePath, {encoding: "utf-8"}); 
         
-//         // Assert
-//         assert.strictEqual(actual, expected);                        
+        // Assert
+        assert.strictEqual(actual, expected);                        
         
-//         // Tear Down
-//         if (fs.existsSync(filePath)) { fs.unlinkSync(filePath) };
-//     });
+        // Tear Down
+        if (fs.existsSync(filePath)) { fs.unlinkSync(filePath) };
+    });
 
 
-//     it('Can READ a file', async function() {
-//         // Arrange
-//         const mockData = {
-//             swisCode:"123456",propertyLocationStreetNumber:"1773",propertyLocationStreetName:"1222",propertyLocationCityTown:"122ddd",propertyLocationVillage:"1133ss",propertyLocationZipcode:"1",buyerLastNameCompanyOne:"145",buyerFirstNameOne:"1",buyerLastNameCompanyTwo:"1sssss",buyerFirstNameTwo:"1",taxAddressBuyerLastNameCompany:"1",taxAddressBuyerFirstName:"11",taxAddressStreetNumberAndName:"122",taxAddressCityTown:"",taxAddressState:"11",taxAddressZipCode:"",fourNumberOfParcels:"4",fourPartOfParcelCheckbox:false,fourFrontFeet:"1",fourDepth:"1",fourAcres:"1",fourSubDivAuthExists:false,fourSubDivApprovalRequired:false,fourParcelApprovedWithMap:false,sellerNameLastNameCompany:"1",sellerNameFirstName:"1",sellerNameLastNameCompanyTwo:"1",sellerNameFirstNameTwo:"1",propertyUseSelect:"3",ownershipIsCondo:null,constructionOnVacant:null,locatedWithinAg:null,buyerReceivedDisclosureNotice:null,saleContractDate:"0001-01-01",saleTransferDate:"0001-01-01",salePrice:"12,222,222",salePersonalPropertyVal:"222,222",saleConditionComments:"1",saleInfoCheckA:false,saleInfoCheckB:false,saleInfoCheckC:false,saleInfoCheckD:false,saleInfoCheckE:false,saleInfoCheckF:false,saleInfoCheckG:false,saleInfoCheckH:false,saleInfoCheckI:false,saleInfoCheckJ:true,assessmentRollYear:"12",assessmentPropClassFirstInput:"100",assessmentTotalValue:"33,333,333",assessmentSchoolDistrict:"1",taxMapIdOne:"1",taxMapIdTwo:"1",taxMapIdThree:"1",taxMapIdFour:"1",contactInfoLastName:"1",contactInfoFirstName:"1",contactInfoAreaCode:"133",contactInfoPhoneNumber:"133-3333",contactInfoStreetNumber:"1",contactInfoStreetName:"1",contactInfoCityTown:"1",contactInfoState:"1",contactInfoZipCode:"1",contactAttorneyInfoLastName:"1",contactInfoAttorneyFirstName:"1",contactInfoAttorneyAreaCode:"133",contactInfoAttorneyPhoneNum:"133-3333",validationCounterForNumberOfParcels:false
-//         };
-//         const filePath = path.resolve(__dirname, 'fileForReadTest.sdg');
-//         let actual;
-//         const expected = JSON.stringify(mockData);
-//         let res = {};
-//         fs.writeFileSync(filePath, JSON.stringify(mockData), {encoding:"utf-8"});
+    it('Can READ a file', async function() {
+        // Arrange
+        const mockData = {
+            swisCode:"123456",propertyLocationStreetNumber:"1773",propertyLocationStreetName:"1222",propertyLocationCityTown:"122ddd",propertyLocationVillage:"1133ss",propertyLocationZipcode:"1",buyerLastNameCompanyOne:"145",buyerFirstNameOne:"1",buyerLastNameCompanyTwo:"1sssss",buyerFirstNameTwo:"1",taxAddressBuyerLastNameCompany:"1",taxAddressBuyerFirstName:"11",taxAddressStreetNumberAndName:"122",taxAddressCityTown:"",taxAddressState:"11",taxAddressZipCode:"",fourNumberOfParcels:"4",fourPartOfParcelCheckbox:false,fourFrontFeet:"1",fourDepth:"1",fourAcres:"1",fourSubDivAuthExists:false,fourSubDivApprovalRequired:false,fourParcelApprovedWithMap:false,sellerNameLastNameCompany:"1",sellerNameFirstName:"1",sellerNameLastNameCompanyTwo:"1",sellerNameFirstNameTwo:"1",propertyUseSelect:"3",ownershipIsCondo:null,constructionOnVacant:null,locatedWithinAg:null,buyerReceivedDisclosureNotice:null,saleContractDate:"0001-01-01",saleTransferDate:"0001-01-01",salePrice:"12,222,222",salePersonalPropertyVal:"222,222",saleConditionComments:"1",saleInfoCheckA:false,saleInfoCheckB:false,saleInfoCheckC:false,saleInfoCheckD:false,saleInfoCheckE:false,saleInfoCheckF:false,saleInfoCheckG:false,saleInfoCheckH:false,saleInfoCheckI:false,saleInfoCheckJ:true,assessmentRollYear:"12",assessmentPropClassFirstInput:"100",assessmentTotalValue:"33,333,333",assessmentSchoolDistrict:"1",taxMapIdOne:"1",taxMapIdTwo:"1",taxMapIdThree:"1",taxMapIdFour:"1",contactInfoLastName:"1",contactInfoFirstName:"1",contactInfoAreaCode:"133",contactInfoPhoneNumber:"133-3333",contactInfoStreetNumber:"1",contactInfoStreetName:"1",contactInfoCityTown:"1",contactInfoState:"1",contactInfoZipCode:"1",contactAttorneyInfoLastName:"1",contactInfoAttorneyFirstName:"1",contactInfoAttorneyAreaCode:"133",contactInfoAttorneyPhoneNum:"133-3333",validationCounterForNumberOfParcels:false
+        };
+        const filePath = path.resolve(__dirname, 'fileForReadTest.sdg');
+        let actual;
+        const expected = JSON.stringify(mockData);
+        let res = {};
+        fs.writeFileSync(filePath, JSON.stringify(mockData), {encoding:"utf-8"});
 
-//         // Act
-//         await readFile( filePath, res )
-//         actual = res.dataFromFile;
+        // Act
+        await readFile( filePath, res )
+        actual = res.dataFromFile;
         
-//         // Assert
-//         assert.strictEqual(actual, expected);
+        // Assert
+        assert.strictEqual(actual, expected);
         
-//         // Tear Down
-//         if (fs.existsSync(filePath)) fs.unlinkSync(filePath)
-//     });
+        // Tear Down
+        if (fs.existsSync(filePath)) fs.unlinkSync(filePath)
+    });
 
-//     return;
-// });
+    return;
+});
