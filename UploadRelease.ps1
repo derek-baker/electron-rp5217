7e8740c662c8037da71c59e9208e6f11258628eb
dbaker@sdgnys.com <dbaker@sdgnys.com>
2019-03-15 11:37:29 -0400

    Refactor

--- UploadRelease.ps1
+++ UploadRelease.ps1
@@ -1,4 +1,7 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop";
+
 # INSTALL GCP SDG: https://cloud.google.com/sdk/docs/#windows
 # https://cloud.google.com/storage/docs/quickstart-gsutil
 
-gsutil cp ".\electron_dist\RP5217 Setup.exe" gs://sdg_installers_release
+gsutil cp ".\electron_dist\RP5217_Setup.exe" gs://sdg_installers_release
