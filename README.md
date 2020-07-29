e8683d9ff3002d1ac1d7c1a14c1f25adbafd3faa
derek-baker <dbaker@sdgnys.com>
2020-07-29 10:58:46 -0400

    Edit readme.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# After running npm install
+### After running npm install
     run npm postinstall
 
-# INSTALL AND CONFIGURE GCP SDK: https://cloud.google.com/sdk/docs/#windows
+### INSTALL AND CONFIGURE GCP SDK: https://cloud.google.com/sdk/docs/#windows
     https://cloud.google.com/storage/docs/quickstart-gsutil 
     gcloud init 
     OR 
@@ -9,37 +9,35 @@
     gcloud config set project single-odyssey-201216
 
 
-# In the NPM scripts in the package.json, 
+### In the NPM scripts in the package.json, 
     env vars are being set with Windows CMD syntax
 
-# This project uses electron builder 
+### This project uses electron builder 
 https://www.electron.build/configuration/configuration 
 
-# Auto update is set up 
+### Auto update is set up 
     https://www.electron.build/auto-update 
 
     You'll need to set an env var that uses a token before you can publish the app
     (the token is not in the local repo, but it can be regenerated from GitHub if necessary) 
-
-    App uses API endpoint to produce PDFs 
 
     App uses API endpoint for contacting SDG 
 
     This doesn't appear to work 
     https://electronjs.org/docs/tutorial/notifications#windows 
 
-# WHEN YOU MAKE CHANGES, 
+### WHEN YOU MAKE CHANGES, 
     RUN THE TESTS!!! (npm run test) 
 
-# TO PUBLISH RELEASE:
+### TO PUBLISH RELEASE:
     make sure you have the file: DoNotCommit.ps1 (it's not in source control)
     make changes
     make sure you run the tests
     if the tests pass, increment version number appropriately in package.json, git commit and git push(so that the changes are associated with the release)
-    publish the release: npm run publish (which also runs release-gcp-storage)
+    publish the release: npm run publish
 
-# THE APP LOGS AT: 
+### THE APP LOGS AT: 
     %appdata%\sdg-rp5217\log.log (but you may need to verify that if you change the app name)
 
-# THE npm publish SCRIPT 
+### THE npm publish SCRIPT 
     MAY APPEAR NON-INUITIVE, BUT TRUST IT (it wouldn't hurt to read through it).
--- UploadRelease.ps1
+++ UploadRelease.ps1
@@ -1,26 +1,42 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop";
 
-# Redirecting output allows $ErrorActionPreference to stop execution (Couldn't get it to work with gsutil but that's fine...)
+$originalLocation = Get-Location
+Set-Location $PSScriptRoot
+
+# Redirecting output allows $ErrorActionPreference to stop execution 
+# TODO: get it to work with gsutil but that's fine...
 [string] $UnitTestsCmd = "npm run test 2>'' "
 # NOTE: this task requires some environmental vars to be set for app-signing purposes. Ask a knowledgable party if you're not.
-[string] $PublishTasksCmd = "npm run publish-pre-reqs 2>'' "
+[string] $BuildCmd = "npm run publish-pre-reqs 2>'' "
 
-# INSTALL GCP SDG: https://cloud.google.com/sdk/docs/#windows
+# INSTALL gcloud and gsutil: https://cloud.google.com/sdk/docs/#windows
 # https://cloud.google.com/storage/docs/quickstart-gsutil
 # NOTE: the destination arg below is a GCP bucket name so keep track of that
+# NOTE: Other apps (IMO) download the installer from Cloud Storage.
 [string] $GcpExeReleaseCmd = "gsutil cp '.\electron_dist\RP5217_Setup.exe' gs://sdg_installers_release"
 
 [string[]] $Tasks = @( `
     $UnitTestsCmd, `
-    $PublishTasksCmd, `
+    $BuildCmd #, `
     $GcpExeReleaseCmd
 );
 
 [string] $ErrOut = ""
 [string] $StdOut = ""
-foreach($task in $Tasks) {
-    Write-Host -ForegroundColor Green "`nRUNNING COMMAND: $task"
-    # Deliberately dropped '$' from vars below: https://stackoverflow.com/questions/21583850/powershell-manage-errors-with-invoke-expression
-    Invoke-Expression $task -ErrorVariable ErrOut -OutVariable StdOut
+
+try {
+    foreach($task in $Tasks) {
+        Write-Host -ForegroundColor Green "`nRUNNING COMMAND: $task"
+        # Deliberately dropped '$' from vars below: https://stackoverflow.com/questions/21583850/powershell-manage-errors-with-invoke-expression
+        Invoke-Expression $task -ErrorVariable ErrOut -OutVariable StdOut
+    }
 }
+catch {
+    $_
+}
+finally {
+    Set-Location $originalLocation
+}
+
+
